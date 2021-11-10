a1cc9a3da5d23944d8d62a83e0143383d4cc8e88
MaartenVanNimwegen <89460431+MaartenVanNimwegen@users.noreply.github.com>
2021-11-10 10:43:42 +0100

    test

--- programmerenVanGamesInCS/programmerenVanGamesInCS/Form1.Designer.cs
+++ programmerenVanGamesInCS/programmerenVanGamesInCS/Form1.Designer.cs
@@ -36,7 +36,7 @@ namespace programmerenVanGamesInCS
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(350, 86);
+            this.button1.Location = new System.Drawing.Point(350, 131);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;

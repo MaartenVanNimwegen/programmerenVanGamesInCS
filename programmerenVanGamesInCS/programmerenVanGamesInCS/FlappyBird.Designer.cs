58b72d06f8558f6315b917cf639b0785e6eafa97
MaartenVanNimwegen <89460431+MaartenVanNimwegen@users.noreply.github.com>
2021-11-11 14:35:59 +0100

    pull commit

--- programmerenVanGamesInCS/programmerenVanGamesInCS/FlappyBird.Designer.cs
+++ programmerenVanGamesInCS/programmerenVanGamesInCS/FlappyBird.Designer.cs
@@ -29,10 +29,19 @@ namespace programmerenVanGamesInCS
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FlappyBird
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "FlappyBird";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form2";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
         }
 
         #endregion

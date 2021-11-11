081ca6fe89290f982bbf79739c47305239e2a539
MaartenVanNimwegen <89460431+MaartenVanNimwegen@users.noreply.github.com>
2021-11-11 14:28:33 +0100

    kleine veranderingen homescreen

--- programmerenVanGamesInCS/programmerenVanGamesInCS/homescreen.Designer.cs
+++ programmerenVanGamesInCS/programmerenVanGamesInCS/homescreen.Designer.cs
@@ -39,10 +39,10 @@ namespace programmerenVanGamesInCS
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ControlDark;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(355, 60);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(430, 74);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(196, 75);
+            this.button1.Size = new System.Drawing.Size(261, 92);
             this.button1.TabIndex = 0;
             this.button1.Text = "Speel Lingo";
             this.button1.UseVisualStyleBackColor = false;
@@ -52,10 +52,10 @@ namespace programmerenVanGamesInCS
             // 
             this.button2.BackColor = System.Drawing.SystemColors.ControlDark;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(355, 139);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Location = new System.Drawing.Point(430, 172);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(196, 75);
+            this.button2.Size = new System.Drawing.Size(261, 92);
             this.button2.TabIndex = 1;
             this.button2.Text = "Speel Pong";
             this.button2.UseVisualStyleBackColor = false;
@@ -65,10 +65,10 @@ namespace programmerenVanGamesInCS
             // 
             this.button3.BackColor = System.Drawing.SystemColors.ControlDark;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(355, 218);
-            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Location = new System.Drawing.Point(430, 268);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(196, 75);
+            this.button3.Size = new System.Drawing.Size(261, 92);
             this.button3.TabIndex = 2;
             this.button3.Text = "Speel Flappy Bird";
             this.button3.UseVisualStyleBackColor = false;
@@ -78,23 +78,24 @@ namespace programmerenVanGamesInCS
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
-            this.label1.Location = new System.Drawing.Point(39, 60);
+            this.label1.Location = new System.Drawing.Point(59, 74);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(151, 29);
+            this.label1.Size = new System.Drawing.Size(186, 36);
             this.label1.TabIndex = 3;
             this.label1.Text = "High Scores:";
             // 
             // homescreen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.WindowFrame;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(770, 450);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "homescreen";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
